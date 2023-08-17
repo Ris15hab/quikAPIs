e7ad0f5c7132bab645c5bff694e4a06f8290d9ff
Rishab <rishabpendam@gmail.com>
2023-08-17 13:50:46 +0530

    delete files added

--- controllers/generateAPI.js
+++ controllers/generateAPI.js
@@ -49,11 +49,11 @@ const createCRUD = async (req, res, next) => {
         await routeTemplate(userID, modelFileName, controllerFilePath, routeFilePath)
 
         //adding allroutes
-        const allRoutesNewData = `\n${modelFileName}_${userID}: require('../${routeFilePath}'),`
+        const allRoutesNewData = `\n${modelFileName}: require('../${routeFilePath}'),`
         addAllRoute(allRoutesNewData)
 
         //adding approutes
-        const appRoutesNewData = `\napp.use(allRoutes.${modelFileName}_${userID});`
+        const appRoutesNewData = `\napp.use(allRoutes.${modelFileName});`
         addAppRoute(appRoutesNewData)
 
         //adding details to userDB model
@@ -61,7 +61,7 @@ const createCRUD = async (req, res, next) => {
         //api url
         let url = `${req.hostname}/${modelFileName}`
 
-        if (req.hostname == "localhost") { modelAPI = `http://${url}` }
+        if (req.hostname == "localhost") { modelAPI = `http://${req.hostname}:${process.env.PORT}/${modelFileName}` }
         else { modelAPI = `https://${url}` }
 
         //getting time
@@ -81,10 +81,10 @@ const createCRUD = async (req, res, next) => {
         //saving model
         const userDB = new UserDB({
             modelName,
-            modelStoredAs:modelFileName,
+            modelStoredAs: modelFileName,
             modelDescription,
             modelAPI,
-            dateTime:formattedDate,
+            dateTime: formattedDate,
             userID
         })
         await userDB.save();
@@ -95,6 +95,90 @@ const createCRUD = async (req, res, next) => {
     }
 }
 
+const deleteCRUD = async (req, res, next) => {
+    try {
+        const { _id } = req.query;
+        const userDB = await UserDB.findOne({ _id })
+        console.log(userDB)
+        const userID = req.user.userData._id
+        
+        await fs.readFile(`${__dirname}/../utils/app-routeData.txt`, 'utf-8', (err, data) => {
+            if (err) {
+                console.error('Error reading app-routeData.txt :', err);
+                return;
+            }
+            // console.log(data)
+            const lineToRemove = `app.use(allRoutes.${userDB.modelStoredAs});`
+            var newData = data.replace(lineToRemove, '');
+            // console.log(newData.trim())
+            newData = newData.trim()
+            fs.writeFile(`${__dirname}/../utils/app-routeData.txt`, newData, (err) => {
+                if (err) {
+                    console.error('Error in adding data to app-routes.js', err);
+                } else {
+                    console.log('app-routes.js successfully updated');
+                }
+            });
+            //adding approutes
+            const appRoutesNewData = ``
+            addAppRoute(appRoutesNewData)
+        })
+
+        await fs.readFile(`${__dirname}/../utils/allRoutesData.txt`, 'utf-8', (err, data) => {
+            if (err) {
+                console.error('Error reading allRoutesData.txt :', err);
+                return;
+            }
+            // console.log(data)
+            const lineToRemove = `${userDB.modelStoredAs}: require('../routes/${userID}/${userDB.modelStoredAs}.js'),`;
+            var newData = data.replace(lineToRemove, '');
+            // console.log(newData.trim())
+            newData = newData.trim()
+            fs.writeFile(`${__dirname}/../utils/allRoutesData.txt`, newData, (err) => {
+                if (err) {
+                    console.error('Error in adding data to allRoutes.js', err);
+                } else {
+                    console.log('allRoutes.js successfully updated');
+                }
+            });
+            //adding allroutes
+            const allRoutesNewData = ``
+            addAllRoute(allRoutesNewData)
+        })
+
+
+        await fs.unlink(`${__dirname}/./${userID}/${userDB.modelStoredAs}.js`, (err) => {
+            if (err) {
+              console.error('Error deleting controller file:', err);
+              return;
+            }
+            console.log('Controller File deleted successfully.');
+        });
+
+        await fs.unlink(`${__dirname}/../models/${userID}/${userDB.modelStoredAs}.js`, (err) => {
+            if (err) {
+              console.error('Error deleting model file:', err);
+              return;
+            }
+            console.log('model File deleted successfully.');
+        });
+
+        await fs.unlink(`${__dirname}/../routes/${userID}/${userDB.modelStoredAs}.js`, (err) => {
+            if (err) {
+              console.error('Error deleting routes file:', err);
+              return;
+            }
+            console.log('routes File deleted successfully.');
+        });
+
+        await UserDB.deleteOne({_id})
+        res.status(200).json({messgae:"database deleted successfully"})
+    } catch (err) {
+        next(err)
+    }
+}
+
 module.exports = {
-    createCRUD
+    createCRUD,
+    deleteCRUD
 }
--- routes/generateAPI.js
+++ routes/generateAPI.js
@@ -1,9 +1,10 @@
-const {createCRUD} = require('../controllers/generateAPI')
+const {createCRUD,deleteCRUD} = require('../controllers/generateAPI')
 const {verifyToken} = require('../middleware/auth')
 const express = require('express')
 
 const router = express.Router()
 
 router.post('/crud/createcrud',verifyToken,createCRUD)
+router.delete('/crud/deletecrud',verifyToken,deleteCRUD)
 
 module.exports = router
