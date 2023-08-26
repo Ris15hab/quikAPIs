a48fc5ff090011126b793c209e9ee960a8553aba
Rishab <rishabpendam@gmail.com>
2023-08-26 20:40:01 +0530

    dropall

--- controllers/guiCRUD.js
+++ controllers/guiCRUD.js
@@ -1,14 +1,32 @@
 const UserDB = require('../models/userDB')
-const {createError} = require('../middleware/error')
+const { createError } = require('../middleware/error')
 
-const getFields = async(req,res,next)=>{
-    try{
-        const {_id} = req.query
+const getFields = async (req, res, next) => {
+    try {
+        const { _id } = req.query
         const userID = req.user.userData._id
-        const userdb = await UserDB.find({_id})
+        const userdb = await UserDB.find({ _id })
         const modelCountPromises = userdb.map(async (item) => {
-            const {modelSchema} = require(`../models/${userID}/${item.modelStoredAs}.js`);
-            res.status(200).json({modelSchema:modelSchema.paths})
+            const { modelSchema } = require(`../models/${userID}/${item.modelStoredAs}.js`);
+            res.status(200).json({ modelSchema: modelSchema.paths })
+        });
+
+        // Wait for all the model count promises to resolve
+        await Promise.all(modelCountPromises);
+    } catch (err) {
+        next(err)
+    }
+}
+
+const dropAll = async (req, res, next) => {
+    try{
+        const { _id } = req.query
+        const userID = req.user.userData._id
+        const userdb = await UserDB.find({ _id })
+        const modelCountPromises = userdb.map(async (item) => {
+            const { model } = require(`../models/${userID}/${item.modelStoredAs}.js`);
+            const response = await model.deleteMany({})
+            res.status(200).json({message:"all documents succefully dropped"})
         });
 
         // Wait for all the model count promises to resolve
@@ -19,5 +37,6 @@ const getFields = async(req,res,next)=>{
 }
 
 module.exports = {
-    getFields
+    getFields,
+    dropAll
 }
--- routes/guiCRUD.js
+++ routes/guiCRUD.js
@@ -1,8 +1,9 @@
-const {getFields} = require('../controllers/guiCRUD')
-const {verifyToken} = require('../middleware/auth')
+const { getFields, dropAll } = require('../controllers/guiCRUD')
+const { verifyToken } = require('../middleware/auth')
 const express = require('express')
 const router = express.Router();
 
-router.get('/guiCRUD/getFields',verifyToken,getFields)
+router.get('/guiCRUD/getFields', verifyToken, getFields)
+router.delete('/guiCRUD/dropAll',verifyToken,dropAll)
 
 module.exports = router
