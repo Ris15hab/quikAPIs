51c98eeafaf40a5b9fc3747000fd1070562d475a
Rishab Pendam <rishabpendam@Rishabs-MacBook-Air.local>
2023-08-28 20:46:14 +0530

    fix

--- controllers/generateAPI.js
+++ controllers/generateAPI.js
@@ -1,11 +1,13 @@
 const { createError } = require('../middleware/error')
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 const { modelTemplate } = require('../utils/modelTemplate')
 const { controllerTemplate } = require('../utils/controllerTemplate')
 const { routeTemplate } = require('../utils/routeTemplate')
 const { addAllRoute, addAppRoute } = require('../utils/addRoute')
 const UserDB = require('../models/userDB')
+const fs = require('fs').promises;
+const util = require('util');
 
 const createCRUD = async (req, res, next) => {
     try {
@@ -14,9 +16,20 @@ const createCRUD = async (req, res, next) => {
 
         //User's Model Folder
         const userModelFolder = path.join(__dirname, `../models/${userID}`);
-        if (!fs.existsSync(userModelFolder)) {
-            fs.mkdirSync(userModelFolder);
+        // if (!fs.existsSync(userModelFolder)) {
+        //     await fs.mkdir(userModelFolder);
+        // }
+        try {
+            await fs.access(userModelFolder);
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                await fs.mkdir(userModelFolder, { recursive: true });
+            } else {
+                // Handle other error cases
+                next(error);
+            }
         }
+        
 
         //get time
         const currentTime = new Date();
@@ -32,21 +45,43 @@ const createCRUD = async (req, res, next) => {
 
         //User's Controller Folder
         const userControllerFolder = path.join(__dirname, `./${userID}`);
-        if (!fs.existsSync(userControllerFolder)) {
-            fs.mkdirSync(userControllerFolder);
+        // if (!fs.existsSync(userControllerFolder)) {
+        //     await fs.mkdir(userControllerFolder);
+        // }
+        try {
+            await fs.access(userControllerFolder);
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                await fs.mkdir(userControllerFolder, { recursive: true });
+            } else {
+                // Handle other error cases
+                next(error);
+            }
         }
+        
 
         const controllerFilePath = `controllers/${userID}/${modelFileName}.js`
-        await controllerTemplate(modelFileName, modelSchema, modelFilePath, controllerFilePath)
+        controllerTemplate(modelFileName, modelSchema, modelFilePath, controllerFilePath)
 
         //User's Route Folder
         const userRouteFolder = path.join(__dirname, `../routes/${userID}`);
-        if (!fs.existsSync(userRouteFolder)) {
-            fs.mkdirSync(userRouteFolder);
+        // if (!fs.existsSync(userRouteFolder)) {
+        //     await fs.mkdir(userRouteFolder);
+        // }
+        try {
+            await fs.access(userRouteFolder);
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                await fs.mkdir(userRouteFolder, { recursive: true });
+            } else {
+                // Handle other error cases
+                next(error);
+            }
         }
+        
 
         const routeFilePath = `routes/${userID}/${modelFileName}.js`
-        await routeTemplate(userID, modelFileName, controllerFilePath, routeFilePath)
+        routeTemplate(userID, modelFileName, controllerFilePath, routeFilePath)
 
         //adding allroutes
         const allRoutesNewData = `\n${modelFileName}: require('../${routeFilePath}'),`
@@ -91,6 +126,7 @@ const createCRUD = async (req, res, next) => {
 
         res.status(200).json({ message: "success" })
     } catch (err) {
+        console.log(err)
         next(err)
     }
 }
@@ -102,7 +138,8 @@ const deleteCRUD = async (req, res, next) => {
         console.log(userDB)
         const userID = req.user.userData._id
         
-        await fs.readFile(`${__dirname}/../utils/app-routeData.txt`, 'utf-8', (err, data) => {
+        const appRoutePath = path.join(__dirname,`../utils/app-routeData.txt`)
+        await fs.readFile(appRoutePath, 'utf-8', (err, data) => {
             if (err) {
                 console.error('Error reading app-routeData.txt :', err);
                 return;
@@ -124,7 +161,8 @@ const deleteCRUD = async (req, res, next) => {
             addAppRoute(appRoutesNewData)
         })
 
-        await fs.readFile(`${__dirname}/../utils/allRoutesData.txt`, 'utf-8', (err, data) => {
+        const allRoutePath = path.join(__dirname,`../utils/allRoutesData.txt`)
+        await fs.readFile(allRoutePath, 'utf-8', (err, data) => {
             if (err) {
                 console.error('Error reading allRoutesData.txt :', err);
                 return;
@@ -146,37 +184,56 @@ const deleteCRUD = async (req, res, next) => {
             addAllRoute(allRoutesNewData)
         })
 
+        const controllerPath = path.join(__dirname, `./${userID}/${userDB.modelStoredAs}.js`);
+        await fs.unlink(controllerPath);
 
-        await fs.unlink(`${__dirname}/./${userID}/${userDB.modelStoredAs}.js`, (err) => {
-            if (err) {
-              console.error('Error deleting controller file:', err);
-              return;
-            }
-            console.log('Controller File deleted successfully.');
-        });
+        const modelPath = path.join(__dirname, `../models/${userID}/${userDB.modelStoredAs}.js`);
+        await fs.unlink(modelPath);
 
-        await fs.unlink(`${__dirname}/../models/${userID}/${userDB.modelStoredAs}.js`, (err) => {
-            if (err) {
-              console.error('Error deleting model file:', err);
-              return;
-            }
-            console.log('model File deleted successfully.');
-        });
+        const routePath = path.join(__dirname, `../routes/${userID}/${userDB.modelStoredAs}.js`);
+        await fs.unlink(routePath);
 
-        await fs.unlink(`${__dirname}/../routes/${userID}/${userDB.modelStoredAs}.js`, (err) => {
-            if (err) {
-              console.error('Error deleting routes file:', err);
-              return;
-            }
-            console.log('routes File deleted successfully.');
-        });
-
-        await UserDB.deleteOne({_id})
-        res.status(200).json({messgae:"database deleted successfully"})
+        await UserDB.deleteOne({ _id });
+        res.status(200).json({ message: "database deleted successfully" });
     } catch (err) {
         next(err)
     }
 }
+
+// const deleteCRUD = async (req, res, next) => {
+//     try {
+//         const { _id } = req.query;
+//         const userDB = await UserDB.findOne({ _id });
+//         const userID = req.user.userData._id;
+
+//         const appRoutePath = path.join(__dirname, `../utils/app-routeData.txt`);
+//         let data = await fs.readFile(appRoutePath, 'utf-8');
+//         const lineToRemove = `app.use(allRoutes.${userDB.modelStoredAs});`;
+//         const newData = data.replace(lineToRemove, '');
+//         await fs.writeFile(`${__dirname}/../utils/app-routeData.txt`, newData);
+
+//         const allRoutePath = path.join(__dirname, `../utils/allRoutesData.txt`);
+//         data = await fs.readFile(allRoutePath, 'utf-8');
+//         const lineToRemoveAllRoutes = `${userDB.modelStoredAs}: require('../routes/${userID}/${userDB.modelStoredAs}.js'),`;
+//         const newDataAllRoutes = data.replace(lineToRemoveAllRoutes, '');
+//         await fs.writeFile(`${__dirname}/../utils/allRoutesData.txt`, newDataAllRoutes);
+
+//         const controllerPath = path.join(__dirname, `./${userID}/${userDB.modelStoredAs}.js`);
+//         await fs.unlink(controllerPath);
+
+//         const modelPath = path.join(__dirname, `../models/${userID}/${userDB.modelStoredAs}.js`);
+//         await fs.unlink(modelPath);
+
+//         const routePath = path.join(__dirname, `../routes/${userID}/${userDB.modelStoredAs}.js`);
+//         await fs.unlink(routePath);
+
+//         await UserDB.deleteOne({ _id });
+//         res.status(200).json({ message: "database deleted successfully" });
+//     } catch (err) {
+//         next(err);
+//     }
+// };
+
 
 module.exports = {
     createCRUD,
