d2720d3a0fd024f73e51e9ce19dde4b609138c08
Rishab Pendam <rishabpendam@Rishabs-MacBook-Air.local>
2023-09-03 21:23:41 +0530

    profile-page-integration

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,19 +4,43 @@ import Grid from "@mui/material/Grid";
 import "./Profile.css";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
 import axios from "axios";
 import Chart from "react-apexcharts";
+import Modal from "@mui/material/Modal";
+import { useNavigate } from "react-router-dom";
 
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "55%",
+  transform: "translate(-50%, -50%)",
+  width: 300,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  borderRadius: "15px",
+  border: "none !important",
+  transition: " all .4s",
+};
 
 const Profile = () => {
+  const navigate = useNavigate()
   const [name, setName] = useState("");
+  const [validate, setValidate] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const [profileData, setProfileData] = useState({})
+  const [topDB, setTopDB] = useState([])
+  const [loading, setLoading] = React.useState(false);
   const [donut,setDonut]=useState({series: [1, 1, 1, 1],
     options: {
       colors:["#58ac7b", "#FFB52E","#37BEC1","#c07b71"],
@@ -66,30 +90,74 @@ const Profile = () => {
     ],
   });
 
-  const handleName = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios.get(
-        "http://localhost:8000/user/getUserById",
-        {
-          headers: {
-            authentication: token,
-          },
-        }
-      );
-      setName(response.data.userData.name);
-      //setEmail(response.data.userData.email)
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const handleName = async () => {
+  //   try {
+  //     const token = localStorage.getItem("token");
+  //     const response = await axios.get(
+  //       "http://localhost:8000/user/getUserById",
+  //       {
+  //         headers: {
+  //           authentication: token,
+  //         },
+  //       }
+  //     );
+  //     setName(response.data.userData.name);
+  //     //setEmail(response.data.userData.email)
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
   useEffect(() => {
-    handleName();
+    try{
+      const fetchData = async () => {
+        try {
+          setLoading(true);
+          const token = localStorage.getItem("token");
+          const response = await axios.get("http://localhost:8000/profilePage/getInfo",
+            {
+              headers: {
+                authentication: token,
+              },
+            }
+          );
+          setProfileData(response.data)
+          setName(response.data.userData.username)
+          setTopDB(response.data.top2QuikDbs)
+          setLoading(false);
+        } catch (err) {
+          setValidate("unknown");
+          setOpen(true);
+          setTimeout(() => {
+            setOpen(false);
+          }, 1500);
+        }
+      };
+  
+      fetchData();
+    }catch(err){
+
+    }
   }, []);
 
+  // console.log(profileData.userData.username)
+
   return (
+    <>
     <div>
       <Navbar />
+      {loading ? (
+          <>
+         <div className="loading" style={{marginTop:"30vh",marginLeft:"10vw"}}>
+         <svg width="64px" height="48px">
+             <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
+           <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
+         </svg>
+        </div>
+        <p style={{marginLeft:"10vw"}}>loading...</p>
+        </>
+        ) : (
+          <>
       <Typography
         variant="body1"
         color="initial"
@@ -100,11 +168,10 @@ const Profile = () => {
           fontWeight: "bold",
           paddingLeft: "20vw",
           paddingBottom: "2vh",
-          marginTop: "-1vh",
+          marginTop: "-4vh",
         }}
       >
-        {" "}
-        Mahek's <span style={{ color: "#37BEC1" }}> Profile</span>
+        {name}'s <span style={{ color: "#37BEC1" }}> Profile</span>
       </Typography>
       <Grid container className="grid-profile">
         <Grid item xs={6} md={3} lg={3}>
@@ -126,7 +193,7 @@ const Profile = () => {
                   color: "gray",
                 }}
               >
-                11
+                {profileData.quikDbCount}
               </Typography>
               <Typography
                 sx={{
@@ -158,7 +225,7 @@ const Profile = () => {
                   color: "gray",
                 }}
               >
-                23
+                {profileData.quikApiCount}
               </Typography>
               <Typography
                 sx={{
@@ -211,6 +278,20 @@ const Profile = () => {
         Top<span style={{ color: "#37BEC1" }}> quikDBs</span>
       </Typography>
       <Grid container sx={{ marginTop: "1vh" }}>
+        {(profileData.quikDbCount===0)?(
+          <Typography
+          className="note-head1"
+          variant="body1"
+          color="initial"
+          align="left"
+          sx = {{marginTop:"3vh",marginBottom:"4vh",marginLeft:"50vw !important"}}
+        >
+          {"No Collections to display :( "}
+        </Typography>
+        ):(
+          topDB.map((data)=>{
+
+          return(
         <Grid item xs={12} lg={4.4} md={4.3} className="top-db">
           <Typography
             variant="body1"
@@ -230,7 +311,7 @@ const Profile = () => {
                 marginRight: "1vw",
               }}
             ></i>
-            Users{" "}
+            {data.modelName}
             <i
               className="fa-solid fa-trophy"
               style={{
@@ -250,7 +331,7 @@ const Profile = () => {
               fontFamily: "League Spartan",
             }}
           >
-            Lorem ipsum dolor sit amet consectetur, adipisici....
+            {data.modelDescription}
           </Typography>
           <Typography
             variant="body1"
@@ -264,61 +345,11 @@ const Profile = () => {
               fontFamily: "League Spartan",
             }}
           >
-            No of Entries: 32
+            No of Entries: {data.count}
           </Typography>
         </Grid>
-        <Grid item xs={12} lg={4.4} md={4.3} className="top-db-1">
-          <Typography
-            variant="body1"
-            color="initial"
-            align="left"
-            sx={{
-              paddingLeft: "1vw",
-              fontWeight: "bold",
-              fontFamily: "League Spartan",
-            }}
-          >
-            <i
-              className="fas fa-paper-plane"
-              style={{
-                color: "orange",
-                fontSize: "0.9rem",
-                marginRight: "1vw",
-              }}
-            ></i>
-            Testing{" "}
-            <i
-              className="fa-solid fa-trophy"
-              style={{ fontSize: "12px", color: "silver", marginLeft: "0.4vw" }}
-            ></i>
-          </Typography>
-          <Typography
-            variant="body1"
-            color="initial"
-            align="left"
-            sx={{
-              paddingLeft: "2.8vw",
-              color: "gray",
-              fontFamily: "League Spartan",
-            }}
-          >
-            Lorem ipsum dolor sit amet consectetur, adipisici....
-          </Typography>
-          <Typography
-            variant="body1"
-            color="initial"
-            align="left"
-            sx={{
-              paddingLeft: "2.8vw",
-              color: "#088F8F",
-              fontSize: "0.9rem",
-              paddingTop: "1vh",
-              fontFamily: "League Spartan",
-            }}
-          >
-            No of Entries: 25
-          </Typography>
-        </Grid>
+        )})    
+        )}
       </Grid>
       <Grid container>
         <Grid item xs={12} lg={4.4} md={4.4} sx={{ marginTop: "5vh" }}>
@@ -341,7 +372,24 @@ const Profile = () => {
 
         </Grid>
       </Grid>
+      </>
+        )}
+      {validate=='unknown'&&<Modal
+          open={open}
+          sx={{border:"none !important"}}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h3" sx={{margin:"1vh",fontSize:"1.1rem"}}>
+          <i className="fa-regular fa-circle-xmark" style={{color: "#37bec1",marginRight:"1vw"}}></i>
+          Oops! An Error Occurred<span style={{marginRight:"1vw !important"}}></span>
+          </Typography>
+        </Box>
+      </Modal>
+      }
     </div>
+    </>
   );
 };
 
